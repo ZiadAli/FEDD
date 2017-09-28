3e7f5ae1e73a7dad5ac7ca1f79f797d02c6aff5b
ZiadAli <ziadali147@gmail.com>
2017-09-27 22:02:29 -0400

    Configured project controller to show all teams

---
+++ FEDD/Models/Team.swift
@@ -0,0 +1,17 @@
+//
+//  Team.swift
+//  FEDD
+//
+//  Created by Ziad Ali on 9/27/17.
+//  Copyright © 2017 ZiadCorp. All rights reserved.
+//
+
+import Foundation
+
+class Team:NSObject {
+    
+    var name:String!
+    var project:String!
+    var members:[String]!
+    
+}
